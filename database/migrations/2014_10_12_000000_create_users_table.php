fd6708ac51e429bff306cbb597cc41787d0ae3e0
andiii24 <andiialpha@gmail.com>
2024-01-05 16:14:08 +0300

    updated badge_id to badge

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->intiger('badge_id')->default();
+            $table->intiger('badge')->default();
             $table->rememberToken();
             $table->timestamps();
         });
